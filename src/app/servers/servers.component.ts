acbe891e663512988160b4af665e5d70dfff3e02
Krzysztof Bożek <90691419+Savetheprogram@users.noreply.github.com>
2023-05-11 09:54:57 +0200

    Add files via upload

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,11 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-            <app-server></app-server>
-            <app-server></app-server>`,
+  // template: `
+  //           <app-server></app-server>
+  //           <app-server></app-server>`,
+  templateUrl: 'servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Test server';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
+  onCreateServer() {
+    this.serverCreationStatus ='Server was created! Name is: ' + this.serverName;
+  }
+  onUpdateServerName(event: Event) {
+    this.serverName =(<HTMLInputElement>event.target).value;
+  }
 }
