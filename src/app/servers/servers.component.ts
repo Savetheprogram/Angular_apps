63798c22e22b9487ad4baefae79ff59543632641
Krzysztof Bożek <90691419+Savetheprogram@users.noreply.github.com>
2023-05-16 01:21:04 +0200

    Add files via upload

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,6 +12,7 @@ export class ServersComponent {
   allowNewServer = false
   serverCreationStatus = 'No server was created!';
   serverName = 'Test server';
+  serverCreated = false;
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -19,6 +20,7 @@ export class ServersComponent {
   }
 
   onCreateServer() {
+    this.serverCreated = true
     this.serverCreationStatus ='Server was created! Name is: ' + this.serverName;
   }
   onUpdateServerName(event: Event) {
